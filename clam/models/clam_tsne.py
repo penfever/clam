7d8cf704460bef503d0954bedf24e48eb23eeacd
Ben Feuer <penfever@hotmail.com>
2025-06-27 17:37:52 -0400

    device debug

--- clam/models/clam_tsne.py
+++ clam/models/clam_tsne.py
@@ -65,7 +65,7 @@ class BioClip2EmbeddingExtractor:
     def __init__(self, model_name: str = "hf-hub:imageomics/bioclip-2", device: str = "auto"):
         from clam.utils.device_utils import detect_optimal_device
         self.model_name = model_name
-        self.device = device if device != "auto" else detect_optimal_device()
+        self.device = detect_optimal_device()
         self.model = None
         self.preprocess = None
         self.logger = logging.getLogger(__name__)
